8d60fbf50ff834dd16075779732f132534d8755f
Mykhailo Antoniuk <mantoniuk@n-ix.com>
2021-08-23 13:54:53 +0300

    init commit

---
+++ src/main/java/com/coolservice/backendangularandspringsecurity/BackEndAngularAndSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.coolservice.backendangularandspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAngularAndSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndAngularAndSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coolservice/backendangularandspringsecurity/BackEndAngularAndSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolservice.backendangularandspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAngularAndSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
